929302f7ef7aa2cfa51b33d47f227ddb51793224
Tajudeen Abdullateef <abdullatteeff@gmail.com>
2022-07-04 07:08:43 -0400

    first-three commits added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main -> prints the alphabet in lowercase, followed by a new line.
+*Return : Always 0, non-zero otherwise
+*/
+
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+	putchar(ch);
+}
+putchar(10);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is a readme for the second assignment in C_programming
