7c657fdfe690a77d64643d27240daf842591f180
Tajudeen Abdullateef <abdullatteeff@gmail.com>
2022-07-05 10:16:07 -0400

    first task committed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*main - rints _putchar, followed by a new line.
+*Return: Always 0; non-zero otherwise
+*/
+
+int main(void)
+{
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+return (0);
+}
