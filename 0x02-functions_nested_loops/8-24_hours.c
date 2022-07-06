17b8614cc38d9f943aa39f1a5bec7da954a07aad
Tajudeen Abdullateef <abdullatteeff@gmail.com>
2022-07-06 08:13:31 -0400

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,40 @@
 #include "main.h"
 
 /**
-*_abs - computes the absolute value of an integer.
-*@n: is an integer
+*jack_bauer - function that prints every minute of the day of Jack Bauer,
+*starting from 00:00 to 23:59.
 *Return: Always n
 */
 
-int _abs(int n)
+void jack_bauer(void)
 {
-	if (n < 0)
-	n = -n;
-	return (n);
+	int j;
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+				_putchar('0');
+				_putchar(i + '0');
+		}
+		else if
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		if (j < 10)
+		{
+			_putchar(':');
+			_putchar('0');
+			_putchar(j + '0');
+		}
+		else if
+		{
+			_putchar(':);
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+	}
 }
