9c27856804e0ebf9ae47e4feb170c60222e78ddf
Tajudeen Abdullateef <abdullatteeff@gmail.com>
2022-07-06 02:14:12 -0400

    2-print_alphabet_x10.c hot-fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,12 @@ void print_alphabet_x10(void)
 	int j;
 	int n;
 
-	while (n <= 10)
+	for (n = 0; n < 10; n++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j * 10);
 		}
-		n++;
 	}
 	_putchar('\n');
 }
