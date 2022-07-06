466aab72db238bbf6096782918abba0c08cdace5
Tajudeen Abdullateef <abdullatteeff@gmail.com>
2022-07-06 06:31:34 -0400

    7-print_last_digit.c hot fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,15 +11,8 @@ int print_last_digit(int n)
 	int x;
 
 	if (n < 0)
-	{
 		n = -n;
-		x = n % 10;
-		_putchar('0' + x);
-		return (x);
-	}
-	else
-	{
+	x = n % 10;
 	_putchar('0' + x);
 	return (x);
-	}
 }
