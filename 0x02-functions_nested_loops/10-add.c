76decdc6053ddbd5194607706c2a0304b5ad928a
Tajudeen Abdullateef <abdullatteeff@gmail.com>
2022-07-07 05:37:54 -0400

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*add -> adds two integers and returns the result.
+*Return: Always return 0
+*/
+
+int add(int a, int b)
+{
+	int add;
+
+	add = a + b;
+	_putchar("%d\n", add);
+	return (0);
+}
