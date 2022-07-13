a8f4dbb596108ab37707c4700f7a3024e100ca7f
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-13 21:45:09 +0100

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+* rev_string -> prints a string, in reverse,
+* followed by a new line.
+* @s: string to be passed
+*/
+
+void rev_string(char *s)
+{
+	char tmp;
+	int i, l1, l2;
+	
+	l1 = 0;
+	l2 = 0;
+	while (s[l1] != 0)
+		l1++;
+	
+	l2 = l1 -1;
+	for (i = 0; i < l1 / 2; i++)
+	{
+		tmp =s[i];
+		s[i] = s[l2];
+		s[l2] = tmp;
+		l2 -= 1;
+	}
+}
