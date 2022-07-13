7d093bbf67a5568cc851b9cbc8933f20f2702e2a
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-13 22:30:14 +0100

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,12 +1,21 @@
 #include "main.h"
 #include <string.h>
 /**
-* puts2 -> prints every other character of a string,
+* puts_half -> prints every other character of a string,
 * starting with he first character
 * followed by a new line.
 * @str: string to be passed
 */
 void puts_half(char *str)
 {
-	
+	int i, x, y;
+
+	x = strlen(str);
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2;
+	for (i = y; i < x; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
