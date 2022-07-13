a6564304edf93acda549e02139025ea6cf6fc050
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-13 18:00:53 +0100

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _puts -> prints a string, followed by a new line, to stdout.
+* @str: character pointer to be passed.
+*/
+
+void _puts(char *str)
+{
+	for (; s != '\0'; s++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
