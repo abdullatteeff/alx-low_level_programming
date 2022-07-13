9dd88d512c4e2d84fa036cc2bb070da558a2273a
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-13 21:46:15 +0100

    5-rev_string.c fixed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,16 +10,16 @@ void rev_string(char *s)
 {
 	char tmp;
 	int i, l1, l2;
-	
+
 	l1 = 0;
 	l2 = 0;
 	while (s[l1] != 0)
 		l1++;
-	
-	l2 = l1 -1;
+
+	l2 = l1 - 1;
 	for (i = 0; i < l1 / 2; i++)
 	{
-		tmp =s[i];
+		tmp = s[i];
 		s[i] = s[l2];
 		s[l2] = tmp;
 		l2 -= 1;
