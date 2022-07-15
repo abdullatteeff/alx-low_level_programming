18282b30687d906b6de1a45d69a446e8e8a6369b
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-15 23:32:52 +0100

    hot fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,14 +6,19 @@
 */
 void print_diagonal(int n)
 {
-	int x;
+	int i, x;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
 		for (x = 0; x < n; x++)
+		{
+			for (i = 0; i <= x; i++)
+				_putchar(' ');
 			_putchar(92);
+			_putchar('\n');
+		}
 		_putchar('\n');
 	}
 }
