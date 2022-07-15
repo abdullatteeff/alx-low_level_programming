20c05833a0c60bd2e1e6fc4f62398e6c7e0c268f
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-15 22:28:50 +0100

    hot fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,12 @@
 */
 void more_numbers(void)
 {
-	int x = 0;
+	int x, y;
 
-	while (x <= 10)
-		for (; x <= 14; x++)
-		{
-			_putchar(x + '0');
-		}
-		x++;
-		_putchar('\n');
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		_putchar(y + '0');
+	}
+	_putchar('\n');
 }
