aa52f92135946621ad67e97f1be5413e502cb492
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-15 21:49:38 +0100

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _isdigit -  checks for uppercase character.
+* @c: a int parameter
+* Return: 1, 0 otherwise
+*/
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
