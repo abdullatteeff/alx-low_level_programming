1af84b611b1363bff063daba594c0e45aa111551
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-15 23:19:43 +0100

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_diagonal -  prints more lines
+* @n: an int parameter
+*/
+void print_diagonal(int n);
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+			_putchar(92);
+		_putchar('\n');
+	}
+}
