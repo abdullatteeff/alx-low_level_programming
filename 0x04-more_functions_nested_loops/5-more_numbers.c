f26e8d78e659a7205221ce775e7e8e7d6e3f6219
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-15 22:58:06 +0100

    6-print_line.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,12 @@ void more_numbers(void)
 	{
 		for (y = 0; y < 15; y++)
 		{
+			if (y >= 10)
+			{
+				_putchar((y / 10) + '0');
+				_putchar((y % 10) + '0');
 			_putchar(y + '0');
+			}
 		}
 		x++;
 		_putchar('\n');
