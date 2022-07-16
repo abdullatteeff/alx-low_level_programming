645b3ee0a03d10aaee4019b0a4a50898d1bbecec
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-16 14:42:39 +0100

    hot fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,12 +10,14 @@ void fizz_buzz(void)
 
 	for (x = 0; x <= 100; x++)
 	{
-		if (x % 3 == 0)
-			printf("Fizz ");
-		if (x % 5 == 0)
-			printf("Buzz ");
-		else if (x % 3 == 0 && x % 5 == 0)
+		if (x % 3 == 0 && x % 5 == 0)
 			printf("FizzBuzz ");
+		else if (x % 3 == 0)
+			printf("Fizz ");
+		else if (x % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("d", x);
 		printf("\n");
 	}
 }
