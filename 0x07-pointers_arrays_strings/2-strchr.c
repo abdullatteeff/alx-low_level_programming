42a8f18f57168ea319977bfd712cec41cf3f342a
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-19 22:28:44 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strchr -> function that copies memory area.
+ * @s: starting address for the value to be copied
+ * @c: value to be filled
+ * Return:  a pointer to the first occurrence of the character c
+ * in the string s, or NULL if the character is not found
+*/
+char *_strchr(char *s, char c)
