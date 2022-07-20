a651a4b90ad98d6f544e2b3a7594efb7a21dfe7a
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-20 11:56:18 +0100

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strstr ->  
+ * @haystack: starting address for the value to be copied
+ * @needle: value to be filled
+ * Return:  ointer to the beginning of the located substring
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(s, accept));
+}
