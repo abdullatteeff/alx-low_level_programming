a3e7d9644de32ef43de86e83b36d7e52c5a1a6ba
Abdullateef Tajudeen <abdullatteeff.com>
2022-07-23 09:48:34 +0100

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: parameter1
 */
 void _puts_recursion(char *s)
 {
-	_puts("The recursion project");
+	puts("The recursion project");
 }
