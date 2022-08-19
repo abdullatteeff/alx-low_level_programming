e581abf3992fa7d011438c80b1539d7391ce85cd
Abdullateef Tajudeen <abdullatteeff.com>
2022-08-19 10:42:33 +0100

    copied project

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it
+ * @argc: number of arguments
+ * @argv: array that contains the arguments
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
